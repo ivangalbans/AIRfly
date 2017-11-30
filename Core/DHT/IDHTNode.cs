843283d0216f39b98439e650cd6337920946f87d
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-11-30 00:56:38 -0500

    add interface IDHTNode and created ChordNode

---
+++ Core/DHT/IDHTNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DHT
+{
+    public interface IDHTNode
+    {
+        string Host { get; set; }
+        int Port { get; set; }
+        ulong ID { get; set; }
+    }
+}
---
+++ DHTChord/Node/ChordNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Core.DHT;
+
+namespace DHTChord.Node
+{
+    class ChordNode : IDHTNode
+    {
+        public string Host { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Port { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ulong ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}

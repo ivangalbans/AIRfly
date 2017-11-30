68716d226694e15b447bee375994dc0a05f1f36e
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-11-30 02:25:01 -0500

    add GetState to ChordNode

--- DHTChord/Node/ChordNode.cs
+++ DHTChord/Node/ChordNode.cs
@@ -5,13 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Core.DHT;
-
+using DHTChord.State;
 namespace DHTChord.Node
 {
-    class ChordNode : IDHTNode
+    public class ChordNode : IDHTNode
     {
         public string Host { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int Port { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public ulong ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ChordState GetState()
+        {
+            if(this == null)
+            {
+                throw new Exception("Invalid Node");
+            }
+            try
+            {
+                return (ChordState)Activator.GetObject(typeof(ChordState), $"tcp://{Host} : {Port}/chord");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
     }
 }

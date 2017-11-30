c6b50a2c3d3e4ca247977dfb984173e047d1c17f
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-11-30 02:25:59 -0500

    add interface IRoutingTable and FingerTable class

---
+++ DHTChord/FingerTable/FingerTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Core.DHT;
+
+namespace DHTChord.Node
+{
+    class FingerTable
+    {
+        public ChordNode[] Successor { get; set; }
+        public ulong[] StartValues { get; set; }
+
+        public FingerTable(ChordNode node)
+        {
+            Successor = new ChordNode[64];
+            StartValues = new ulong[64];
+
+            for(int i = 0; i < Successor.Length; ++i)
+            {
+                Successor[i] = node;
+                StartValues[i] = node.ID + (1UL << i);
+            }
+        }
+    }
+}

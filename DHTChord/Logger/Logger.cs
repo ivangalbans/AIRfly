62695116273829342feaed7d059378303b3c8c0d
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-11-30 06:18:42 -0500

    add Logger class

---
+++ DHTChord/Logger/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHTChord.Logger
+{
+    public static class Logger
+    {
+        public static void Log(string message, params object[] parameters)
+        {
+            Console.WriteLine($"{DateTime.Now} {Server.ChordServer.LocalNode} {string.Format(message,parameters)}");
+        }
+    }
+}

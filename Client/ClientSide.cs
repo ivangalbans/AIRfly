4c9d8be7d566dcdb42b7af85d688ec3e985422ec
abelpenton <2235penton@gmail.com>
2017-12-13 10:30:51 -0800

    working in streaming

---
+++ Client/ClientSide.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DHTChord.Server;
+using System.IO;
+using DHTChord.NodeInstance;
+using DHTChord.Node;
+
+
+namespace Client
+{
+    public static class ClientSide
+    {
+        public static void Send(string fileName, string path, ChordNode node)
+        {
+            var key = ChordServer.GetHash(fileName);
+
+            Stream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            var request = new FileUploadMessage();
+
+            var fileMetadata = new FileMetaData(fileName);
+            request.Metadata = fileMetadata;
+            request.FileByteStream = fileStream;
+
+            var conteinerNode = ChordServer.CallFindContainerKey(node,key);
+
+            ChordServer.Instance(conteinerNode).AddNewFile(request);
+            
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DHTChord.NodeInstance;
+using System.IO;
+using System.Net;
+using DHTChord.Node;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) args = new string[] { "Send", "localhost", "7070" };
+            if(args[0] == "Send")
+            {
+                string host = Dns.GetHostEntry(args[1]).HostName;
+                int port = int.Parse(args[2]);
+                var node = new ChordNode(host, port);
+                //TODO: node = discovery
+            
+                string[] input = Console.ReadLine().Split(' ');
+                if(input[0] == "1")//all directory
+                {
+                    string path = input[1];
+                    var files = Directory.EnumerateFiles(path);
+
+                    foreach (var f in files)
+                    {
+                        string fileName = Path.GetFileName(f);
+
+                        ClientSide.Send(fileName, path, node);
+                    }
+                }
+                if(input[0] == "2")//a single file
+                {
+                    string path = input[1];
+                    string fileName = Path.GetFileName(path);
+
+                    ClientSide.Send(fileName, path, node);
+                }
+            }
+            if(args[0] == "Find")
+            {
+
+            }
+        }
+    }
+}
